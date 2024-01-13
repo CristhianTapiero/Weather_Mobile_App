59db6607aaa6d46f93e9475d6e7e50cc57b5ee2d
CristhianTapiero <cristhiand-tapierop@unilibre.edu.co>
2024-01-13 01:44:10 -0500

    Connecting the API and making the first interface with a functional search bar and an option menu

--- App.tsx
+++ App.tsx
@@ -1,20 +1,120 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, KeyboardAvoidingView } from 'react-native';
+import { SearchBar } from './components/search-bar';
+import { useState, useEffect } from 'react';
+import { Image } from 'expo-image';
 
+type Weather = {
+  location: {
+    name: string,
+    region: string,
+    country: string,
+    lat: number,
+    lon: number,
+    tz_id: string,
+    localtime_epoch: number,
+    localtime: string
+  },
+  current: {
+    last_updated_epoch: number,
+    last_updated: string,
+    temp_c: number,
+    temp_f: number,
+    is_day: number,
+    condition: {
+      text: string,
+      icon: string,
+      code: number
+    },
+    wind_mph: number,
+    wind_kph: number,
+    wind_degree: number,
+    wind_dir: string,
+    pressure_mb: number,
+    pressure_in: number,
+    precip_mm: number,
+    precip_in: number,
+    humidity: number,
+    cloud: number,
+    feelslike_c: number,
+    feelslike_f: number,
+    vis_km: number,
+    vis_miles: number,
+    uv: number,
+    gust_mph: number,
+    gust_kph: number
+  }
+}
+
+
+const apiKey = process.env.EXPO_PUBLIC_API_KEY;
+const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&aqi=no&q=`
 export default function App() {
-  return (
+  const [city, setCity] = useState('Bogota');
+  const [weather, setWeather] = useState<Weather>();
+  const fetchWeather = async() =>{
+    const result = await fetch(url+city)
+    const data = await result.json()
+    setWeather(data)
+    console.log(JSON.stringify(data,null,2))
+  }
+  useEffect(() => {
+    fetchWeather()
+  },[city])
+
+  if(!weather) return(
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <ActivityIndicator size="large" color="#0000ff" />
     </View>
+  )
+
+  const {current, location} = weather
+
+  return (
+    <KeyboardAvoidingView
+    behavior='height'
+    keyboardVerticalOffset={0}
+    enabled={false}
+    >
+      <View style={styles.container}>
+        <SearchBar setter={setCity}/>
+        <Image source={'https:'+current?.condition.icon} style={styles.weatherIcon}/>
+        <Text style={styles.weatherTemp}>{current?.temp_c} °C</Text>
+        <Text style={styles.weatherLocation}>{location?.name}, {location?.country}</Text>
+        <StatusBar style="auto" />
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    display: 'flex',
+    position: 'relative',
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(255,100,2,0.9)',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  weatherIcon: {
+    width: 100,
+    height: 100,
+  },
+  weatherTemp: {
+    fontSize: 50,
+    fontWeight: '500',
+    color: 'white',
+  },
+  weatherDesc: {
+    fontSize: 25,
+    fontWeight: '500',
+    color: 'white',
+    textTransform: 'capitalize'
+  },
+  weatherLocation: {
+    fontSize: 20,
+    fontWeight: '500',
+    color: 'white',
   },
 });
---
+++ components/search-bar.tsx
@@ -0,0 +1,114 @@
+import { View, TextInput, Text, Pressable, StyleSheet, TouchableOpacity, Keyboard } from 'react-native';
+import { useEffect, useState } from 'react';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+export const SearchBar = ({ setter }: { setter: Function }) => {
+    const apiKey = process.env.EXPO_PUBLIC_API_KEY;
+    const api = `http://api.weatherapi.com/v1/search.json?key=${apiKey}&q=`
+    const [place, setPlace] = useState('');
+    const [results, setResults] = useState([]);
+    const handlePress = () => {
+        Keyboard.dismiss()
+        setter(place);
+        setPlace('');
+    }
+    const handleOptionPress = (result: any) => {
+        Keyboard.dismiss()
+        setter(result.name + ', ' + result.country);
+        setPlace('');
+    }
+    const fetchResults = async () => {
+        const result = await fetch(api + place);
+        const json = await result.json();
+        setResults(json);
+    }
+
+    useEffect(() => {
+        fetchResults();
+    }, [place])
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.searchContainer}>
+                <TextInput placeholder='City...' value={place} style={styles.input} onChange={e => { setPlace(e.nativeEvent.text) }} />
+                <TouchableOpacity style={styles.button} onPress={handlePress}>
+                    <FontAwesome name='search' size={16}></FontAwesome>
+                </TouchableOpacity>
+            </View>
+            { place !== '' && 
+                <View style={styles.resultBox} >
+                    {
+                        results.length > 0 ?
+                            <>
+                                {results.map((result: any, index: number) => (
+                                    <Pressable style={styles.option} onPress={() => handleOptionPress(result)}>
+                                        <Text key={index}>{result.name}, {result.country}</Text>
+                                    </Pressable>
+                                ))}
+                            </>
+                            : <Text>No results</Text>
+                    }
+                </View>
+            }
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        top: 50,
+        width: '90%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        rowGap: 5,
+        paddingHorizontal: 20,
+        zIndex: 1000
+    },
+    resultBox: {
+        position: 'relative',
+        width: '100%',
+        backgroundColor: 'rgba(255,255,255,0.5)',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        overflow: 'hidden',
+    },
+    option: {
+        position: 'relative',
+        display: 'flex',
+        width: '100%',
+        height: 40,
+        backgroundColor: 'rgba(255,255,255,0.5)',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    searchContainer: {
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        display: 'flex',
+        flexDirection: 'row',
+        backgroundColor: 'rgba(255,255,255,0.5)',
+        borderRadius: 20,
+    },
+    input: {
+        height: 40,
+        width: '75%',
+        paddingHorizontal: 20,
+        borderRadius: 20,
+        fontSize: 20,
+        fontWeight: '500'
+    },
+    button: {
+        height: 40,
+        width: '25%',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        fontSize: 15,
+        fontWeight: '500'
+    }
+});
